0167204b04968b3c5ef10735c2998bca386e0c3b
Maina Kibe <66884954+Mainakibe@users.noreply.github.com>
2021-04-06 15:03:54 +0300

    added the js code

---
+++ main.js
@@ -0,0 +1,49 @@
+//selecting elements
+const slider=document.querySelectorAll('.slide')
+prevBtn=document.querySelector('.prev')
+nextBtn=document.querySelector('.next')
+current=0
+
+
+//clear the slider
+function reset(){
+    for(let i=0;i<slider.length;i++){
+        slider[i].style.display='none'
+    }
+}
+//init slider
+function slideStater(){
+    reset()
+    slider[0].style.display='flex'
+}
+
+//show prev
+function slidePrev(){
+    reset()
+    slider[current-1].style.display='flex'
+    current--
+}
+//show next
+function slideNext(){
+    reset()
+    slider[current+1].style.display='flex'
+    current++
+}
+
+//prev btn click
+prevBtn.addEventListener('click',function(){
+    if(current===0){
+        current=slider.length;
+    }
+    slidePrev()
+})
+//next btn click
+nextBtn.addEventListener('click',function(){
+    if(current===slider.length-1){
+        current=-1
+    }
+    slideNext()
+})
+
+
+slideStater();
